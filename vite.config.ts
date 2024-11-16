c2df381e75552662c450f1d4bc99853742b011eb
nekoya <ryo.studiom@gmail.com>
2024-11-16 17:32:44 +0900

    Render MDX pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-import build from '@hono/vite-build/cloudflare-pages'
-import adapter from '@hono/vite-dev-server/cloudflare'
-import honox from 'honox/vite'
-import { defineConfig } from 'vite'
+import build from "@hono/vite-build/cloudflare-pages";
+import adapter from "@hono/vite-dev-server/cloudflare";
+import mdx from "@mdx-js/rollup";
+import honox from "honox/vite";
+import remarkFrontmatter from "remark-frontmatter";
+import remarkMdxFrontmatter from "remark-mdx-frontmatter";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [honox({ devServer: { adapter } }), build()]
-})
+  plugins: [
+    honox({ devServer: { adapter } }),
+    mdx({
+      jsxImportSource: "hono/jsx",
+      remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
+    }),
+    build(),
+  ],
+});
