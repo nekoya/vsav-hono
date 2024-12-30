0fc1100a6f09b147940c009bea6531022c65db13
nekoya <ryo.studiom@gmail.com>
2024-12-30 22:01:19 +0900

    npx prettier . --write

--- app/global.d.ts
+++ app/global.d.ts
@@ -3,8 +3,9 @@ import type { Meta } from "./types";
 
 declare module "hono" {
   interface ContextRenderer {
-    (content: string | Promise<string>, meta?: Meta & { frontmatter: Meta }):
-      | Response
-      | Promise<Response>;
+    (
+      content: string | Promise<string>,
+      meta?: Meta & { frontmatter: Meta },
+    ): Response | Promise<Response>;
   }
 }
