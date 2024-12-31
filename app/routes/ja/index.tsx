3d2cfdfe3d6b0f1053a4fd77c325828498d7edbd
nekoya <ryo.studiom@gmail.com>
2024-12-31 17:19:11 +0900

    Update characters

--- app/routes/ja/index.tsx
+++ app/routes/ja/index.tsx
@@ -10,13 +10,21 @@ export default function Top() {
   });
   return (
     <div>
-      <h2>キャラクター</h2>
-      <ul class="article-list">
+      <h2 class="mt-4 text-xl">キャラクター</h2>
+      <ul class="mt-2 flex flex-wrap">
         {Object.entries(characters).map(([id, module]) => {
+          const name = /\/characters\/(.+)\.mdx/.exec(id)?.[1];
           if (module.frontmatter) {
             return (
-              <li>
-                <a href={`${id.replace(/\.mdx$/, "")}`}>
+              <li class="ml-2 mt-2 min-w-32 flex-1">
+                <a
+                  href={`${id.replace(/\.mdx$/, "")}`}
+                  class="flex items-center gap-2 text-indigo-300 underline"
+                >
+                  <img
+                    src={`/static/characters/${name}_lp.gif`}
+                    class="h-8 w-8"
+                  />
                   {module.frontmatter.title}
                 </a>
               </li>
