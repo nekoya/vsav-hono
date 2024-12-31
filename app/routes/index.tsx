1c18055536b2fb086d99b663e6119273ab023338
nekoya <ryo.studiom@gmail.com>
2024-12-31 19:47:21 +0900

    Remove an unused function

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,30 +1,5 @@
 import { createRoute } from "honox/factory";
-import type { Meta } from "../types";
 
 export default createRoute((c) => {
   return c.redirect("/ja/");
 });
-
-function Top() {
-  const posts = import.meta.glob<{ frontmatter: Meta }>("./posts/*.mdx", {
-    eager: true,
-  });
-  return (
-    <div>
-      <h2>Posts</h2>
-      <ul class="article-list">
-        {Object.entries(posts).map(([id, module]) => {
-          if (module.frontmatter) {
-            return (
-              <li>
-                <a href={`${id.replace(/\.mdx$/, "")}`}>
-                  {module.frontmatter.title}
-                </a>
-              </li>
-            );
-          }
-        })}
-      </ul>
-    </div>
-  );
-}
