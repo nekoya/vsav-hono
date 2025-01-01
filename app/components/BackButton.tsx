c6240992b4383f933914aa6319959bb3e10c9716
nekoya <ryo.studiom@gmail.com>
2025-01-01 19:19:47 +0900

    Update styles

--- app/components/BackButton.tsx
+++ app/components/BackButton.tsx
@@ -3,7 +3,7 @@ import type { Child } from "hono/jsx";
 export function BackButton(props: { href: string; children: Child }) {
   return (
     <a href={props.href}>
-      <button class="w-1/2 rounded-md bg-rose-900 p-2 text-sm tracking-widest">
+      <button class="w-64 rounded-md bg-rose-900 p-2 text-sm tracking-widest">
         {props.children}
       </button>
     </a>
