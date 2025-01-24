66df399e6aae253a0da00e7a3c47d458122b2700
nekoya <ryo.studiom@gmail.com>
2025-01-24 20:08:07 +0900

    Do not depend on frontmatter

--- app/routes/ja/commands/_renderer.tsx
+++ app/routes/ja/commands/_renderer.tsx
@@ -1,26 +1,35 @@
-import { jsxRenderer } from "hono/jsx-renderer";
+import { jsxRenderer, useRequestContext } from "hono/jsx-renderer";
 import { BackButton } from "../../../components/BackButton";
-import { getCharacterName } from "../../../domains/character";
+import { characters } from "../../../domains/character";
 
 export default jsxRenderer(({ children, Layout, frontmatter }) => {
-  const name = getCharacterName(frontmatter.name);
+  const c = useRequestContext();
+  const path = c.req.path.split("/").pop();
+  const character = characters.find((v) => v.id === path);
+  if (character === undefined) {
+    return (
+      <Layout title="not found">
+        <div>not found</div>
+      </Layout>
+    );
+  }
   return (
-    <Layout title={`コマンド一覧 ${name}`}>
+    <Layout title={`コマンド一覧 ${character.name}`}>
       <h1 class="mt-4 flex items-center gap-2 text-3xl">
-        <img src={`/static/characters/icons/${frontmatter.name}_lp.gif`} />
-        {name}
+        <img src={`/static/characters/icons/${character.id}_lp.gif`} />
+        {character.name}
       </h1>
       <article class="mt-2">
         <div class="my-4">
-          <a href={`/ja/characters/${frontmatter.name}`}>
+          <a href={`/ja/characters/${character.id}`}>
             &laquo; キャラ紹介に戻る
           </a>
         </div>
         {children}
       </article>
       <div class="my-8 text-center">
-        <BackButton href={`/ja/characters/${frontmatter.name}`}>
-          {name}のキャラ紹介へ
+        <BackButton href={`/ja/characters/${character.id}`}>
+          {character.name}のキャラ紹介へ
         </BackButton>
       </div>
       <div class="my-8 text-center">
