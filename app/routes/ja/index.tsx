de60d2c71cd35866c866c71beaba796b2525fa7d
nekoya <ryo.studiom@gmail.com>
2025-01-24 20:07:11 +0900

    Use static character list

--- app/routes/ja/index.tsx
+++ app/routes/ja/index.tsx
@@ -1,11 +1,7 @@
-import { getCharacterName } from "../../domains/character";
+import { characters } from "../../domains/character";
 import type { Meta } from "../../types";
 
 export default function Top() {
-  const characters = import.meta.glob<{ frontmatter: Meta }>(
-    "./characters/*.mdx",
-    { eager: true },
-  );
   const posts = import.meta.glob<{ frontmatter: Meta }>("./posts/*.mdx", {
     eager: true,
   });
@@ -64,24 +60,21 @@ export default function Top() {
       </ul>
       <h2 class="mt-8 text-xl">キャラクター紹介</h2>
       <ul class="mt-2 flex max-w-3xl flex-wrap">
-        {Object.entries(characters).map(([id, module]) => {
-          if (module.frontmatter) {
-            const name = module.frontmatter.name;
-            return (
-              <li class="ml-2 mt-2 min-w-32 flex-1">
-                <a
-                  href={`./characters/${name}`}
-                  class="flex items-center gap-2 text-indigo-300 underline"
-                >
-                  <img
-                    src={`/static/characters/icons/${name}_lp.gif`}
-                    class="h-8 w-8"
-                  />
-                  {getCharacterName(name)}
-                </a>
-              </li>
-            );
-          }
+        {characters.map((v) => {
+          return (
+            <li class="ml-2 mt-2 min-w-32 flex-1">
+              <a
+                href={`./characters/${v.id}`}
+                class="flex items-center gap-2 text-indigo-300 underline"
+              >
+                <img
+                  src={`/static/characters/icons/${v.id}_lp.gif`}
+                  class="h-8 w-8"
+                />
+                {v.name}
+              </a>
+            </li>
+          );
         })}
       </ul>
     </div>
